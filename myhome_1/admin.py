96ab1e5e2f4f05ee6d452b13a5046ee8533c254a
konstantin-poltavets <Kos@5240>
2020-01-28 21:18:22 +0200

    ignore

--- myhome_1/admin.py
+++ myhome_1/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import mqtt, Payment
+from api.models import Bucketlist
 
 admin.site.register(mqtt)
 admin.site.register(Payment)
+admin.site.register(Bucketlist)
