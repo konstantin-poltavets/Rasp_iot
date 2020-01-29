1a42384b82aaa608c36a4bb34551a81269f64b40
konstantin-poltavets <Kos@5240>
2020-01-29 18:10:24 +0200

    ignore

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '27t0#@(p^8tbogq(hskkghn@&qht+f011z9q)&&uw3bgb1e)@='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
