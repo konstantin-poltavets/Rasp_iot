fc1d7d1185906080a45e3c0d74d219286f74bdf3
konstantin-poltavets <Kos@5240>
2020-04-04 23:35:51 +0300

    ignore

--- iot/static/iot/js/indicator.js
+++ iot/static/iot/js/indicator.js
@@ -22,6 +22,8 @@ function startConnect() {
                 keepAliveInterval: 100});
 console.log("attempting to connect...");
 
+  my_chart_1 = [['time', 'Distance', 'Speed']];
+
 }
 
 // Called when the client connects
@@ -36,6 +38,7 @@ function onConnect() {
 	$("#seven-seg-array_s").sevenSeg({ digits: 2, value: 00, colorOff: "#003200", colorOn: "Lime",  decimalPoint: true});
     $("#seven-seg-array_m").sevenSeg({ digits: 2, value: 00, colorOff: "#003200", colorOn: "Lime",  decimalPoint: true});
     $("#seven-seg-array_h").sevenSeg({ digits: 2, value: 00, colorOff: "#003200", colorOn: "Lime",  decimalPoint: true});
+
 }
 
 // Called when the client loses its connection
@@ -49,49 +52,35 @@ function onConnectionLost(responseObject) {
 // Called when a message arrives
 function onMessageArrived(message) {
 let payload = JSON.parse(message.payloadString);
-
-   // console.log(payload);
     document.getElementById("messages").innerHTML += '<span>Topic: ' + message.destinationName + '  '+ message.payloadString +'| '  + '</span><br/>';
-//let t = payload["time"];
-
 
 	 $("#seven-seg-array").sevenSeg({ digits: 4, value: payload["distance"] });
 	 $("#seven-seg-array_v").sevenSeg({ digits: 4, value: payload["speed"], decimalPoint: true });
 
 let time_s = 0;
 let time_m = 0;
-
 let  time_row = parseInt(payload["time"]);
-
-
 if (time_row  > 59) { time_s = time_row % 60; }
 else { time_s = time_row; }
-
 if (time_s < 10 ){time_s = '0' + time_s.toString();}
-
 let time_mm = Math.floor(time_row / 60);
-
-
 if (time_mm  > 59) { time_m = time_mm % 60; }
 else { time_m = time_mm; }
 
-
 if (time_m < 10  ){time_m = '0' + time_m.toString();}
-
 let time_h = Math.floor(time_row / 60/60);
 
-
-
 	$("#seven-seg-array_h").sevenSeg({ digits: 2, value: time_h.toString() , decimalPoint: true});
-
 	$("#seven-seg-array_m").sevenSeg({ digits: 2, value: time_m.toString() , decimalPoint: true});
-
 	$("#seven-seg-array_s").sevenSeg({ digits: 2, value: time_s.toString() , decimalPoint: true});
 
-document.getElementById('messages').scrollTop = 9999999;
+    document.getElementById('messages').scrollTop = 9999999;
 
-	}
+      my_chart_1.push([payload["time"] ,payload["distance"],payload["speed"]*100]);
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
 
+ };
 
 
 // Called when the disconnection button is pressed
@@ -99,9 +88,12 @@ function startDisconnect() {
     client.disconnect();
     document.getElementById("messages").innerHTML += '<span>Disconnected</span><br/>';
 }
-
-
-
 //	setInterval('show()',1000);
 
 
+function drawChart() {
+        var data = google.visualization.arrayToDataTable(my_chart_1);
+        var options = {title: 'Distance', curveType: 'function', legend: { position: 'bottom' }}
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+        chart.draw(data, options);
+        }
